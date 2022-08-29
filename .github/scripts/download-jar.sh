dcc035e3659bfcb8cdd5bd5a0dbe6fc6ef5b68d0
griggheo <grig@gheorghiu.net>
2022-08-28 21:23:27 -0700

    add step for downloading jar from GH Packages

---
+++ .github/scripts/download-jar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+USERNAME=$1
+PAT=$2
+VERSION=$3
+
+curl -O -L https://${USERNAME}:${PAT}@maven.pkg.github.com/codepraxis-io/spring-music/io/codepraxis/spring-music/${VERSION}/spring-music-${VERSION}-plain.jar
