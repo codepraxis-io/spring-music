44cac4e776bc84bd79ea8c801bcdb67de51707c6
griggheo <grig@gheorghiu.net>
2022-08-28 22:44:01 -0700

    download bootjar and not plain jar

--- .github/scripts/download-jar.sh
+++ .github/scripts/download-jar.sh
@@ -4,4 +4,4 @@ USERNAME=$1
 PAT=$2
 VERSION=$3
 
-curl -O -L https://${USERNAME}:${PAT}@maven.pkg.github.com/codepraxis-io/spring-music/io/codepraxis/spring-music/${VERSION}/spring-music-${VERSION}-plain.jar
+curl -O -L https://${USERNAME}:${PAT}@maven.pkg.github.com/codepraxis-io/spring-music/io/codepraxis/spring-music/${VERSION}/spring-music-${VERSION}.jar
